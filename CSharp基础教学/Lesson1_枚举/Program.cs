bd336b98f597f3ae67a260be50e7934ff0678570
ww <ww@ww>
2023-02-24 10:26:14 +0800

    Lesson1_枚举

---
+++ CSharp基础教学/Lesson1_枚举/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lesson1_枚举
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
