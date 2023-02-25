e3b9064984d19497e72e3df17b1fdd0d032e938b
ww <ww@ww>
2023-02-26 00:28:31 +0800

    lesson4交错数组 无练习题

---
+++ CSharp基础教学/Lesson4_交错数组/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Lesson4_交错数组
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("交错数组");
+
+            int[][] array = {new int[] {1, 2,3},
+                             new int[] {4, 5}};
+
+            //数组的长度-行
+            Console.WriteLine(array.GetLength(0));
+
+            //数组的长度-列
+            Console.WriteLine(array[0].Length);
+        }
+    }
+}
