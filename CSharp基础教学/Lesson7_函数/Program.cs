1655456b4b118c922ea5ac00bfbd009a690f8cb5
ww <ww@ww>
2023-02-27 01:06:52 +0800

    Lesson7 函数及练习题更新

--- CSharp基础教学/Lesson7_函数/Program.cs
+++ CSharp基础教学/Lesson7_函数/Program.cs
@@ -2,9 +2,88 @@
 {
     internal class Program
     {
+        //基本语法
+        //
+        //static 返回类型 函数名(参数类型 参数名1，参数类型 参数名2，……)
+        //{ 
+        //函数的代码逻辑;
+
+        //return 返回值;（如果有返回类型才返回，是否需要写是可选的）
+        //}
+
+
+        //1.无参无返回值函数
+        static void SayHellow()
+        {
+            Console.WriteLine("你好世界");
+            return;//这个可以不写
+        }
+
+        //2.有参无返回值函数
+        static void SayYourName(string name)
+        {
+            Console.WriteLine("你的名字是{0}", name);
+            return;//这个可以不写
+        }
+
+        //3.无参有返回值函数
+        static string WhatYourName()
+        {
+            return "王大锤";
+        }
+        
+        //4.有参有返回值函数
+        static int Sum(int a, int b)
+        {
+            int c = a + b;
+            return c;
+            //上面两句可以直接写成return a + b;
+            //return后面可以跟表达式，只要表达式的结果跟函数返回值的类型一致即可
+        }
+
+        //5.有参有返回值函数
+        static int[] Calc(int a, int b)
+        { 
+            int sum = a + b;
+            int avg = sum / 2;
+            //第一种return方式
+            //int[] arr = new int[] { sum, avg };
+            //return arr;
+
+            //第二种return方式
+            return new int[] { sum, avg };
+
+            //注意不可以写的return方式
+            //return { sum, avg };这种书写方式是错误的
+        }
+
+        static void Speak(string str)
+        {
+            if (str == "混蛋")
+            {
+                return;
+            }
+            Console.WriteLine(str);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("函数");
+            //SayHellow();
+
+            //string name = "王大锤";
+            //SayYourName(name);
+
+            //SayYourName(WhatYourName());
+
+            //string str2 = WhatYourName();
+
+            //WhatYourName();
+
+            //Console.WriteLine(Sum(2, 3)) ;
+
+            int[] arr = Calc(5, 7);
+            Console.WriteLine(arr[0] + "  " + arr[1]);
         }
     }
 }
---
+++ CSharp基础教学/Lesson7_函数练习题/Program.cs
@@ -0,0 +1,155 @@
+﻿namespace Lesson7_函数练习题
+{
+    internal class Program
+    {
+
+        //练习一：比较连个数字大小的函数
+        static int BigNum(int a, int b)
+        {
+            if (a >= b)
+            {
+                return a;
+            }
+            return b;
+        }
+
+        //练习二：求圆的面积和周长，并返回打印
+        static float[] CalcCircle(float r)
+        {
+            const float PI = 3.1415f;
+            return new float[] { 2 * PI * r, PI * r * r };
+        }
+
+        //练习三：求数组的总和、最大、最小、平均值
+        /// <summary>
+        /// 返回数组元素总和、最大、最小、平均值
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        static float[] CalcArray(float[] a)
+        {
+            float sum = 0;
+            float min = 0, max = 0, avg = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                sum += a[i];
+                if (i == 0) 
+                {
+                    min = a[i];
+                    max = a[i];
+                }
+                else
+                {
+                    if (min >= a[i])
+                    {
+                        min = a[i];
+                    }
+                    if (max <= a[i])
+                    { 
+                        max = a[i];
+                    }
+                }
+            }
+            avg = sum / a.Length;
+
+            return new float[] { sum, min, max, avg };
+        }
+
+        //练习四：判断输入是否是质数
+        static void PrimeNumber(uint a)
+        {
+            if (a == 1 || a == 0)
+            {
+                Console.WriteLine("你输入的数字{0}不是质数",a);
+            }
+            else 
+            {
+                bool flag = true;
+                int zhengChu = 0;
+                for (int i = 2; i < a; i++)
+                {
+                    if (a % i == 0 && a != 2)
+                    {
+                        flag = false;
+                        zhengChu = i;
+                        //Console.WriteLine("你输入的数字{0}不是质数，它至少被{1}整除", a, i);
+                        break;
+                    }
+                }
+                if (flag == false)
+                {
+                    Console.WriteLine("你输入的数字{0}不是质数，它至少被{1}整除", a, zhengChu);
+                }
+                else
+                {
+                    Console.WriteLine("你输入的数字{0}是质数", a);
+                }
+            }
+        }
+
+        //练习五：判断是否是闰年
+        static void LeapYear(uint a)
+        {
+            //if (a % 400 == 0 || (a % 4 == 0 && a % 100 != 0))
+            //{
+            //    Console.WriteLine("{0}是闰年", a);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("{0}不是闰年", a);
+            //}
+
+            Console.WriteLine("{0}{1}", a, (a % 400 == 0 || (a % 4 == 0 && a % 100 != 0)) ? "是闰年" : "不是闰年");
+        }
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+
+            //练习一
+            //Console.WriteLine(BigNum(189, 1));
+
+            //练习二
+            //try
+            //{
+            //    float r = float.Parse(Console.ReadLine());
+            //    CalcCircle(r);
+            //    float[] arr = CalcCircle(r);
+            //    Console.WriteLine(arr[0] + "  " + arr[1]);
+
+            //}
+            //catch
+            //{
+            //    Console.WriteLine("请输入数字");
+            //}
+
+            ////练习三
+            //float[] arr3 = { 1.5f, 2.5f, 3.5f };
+            //Console.WriteLine(CalcArray(arr3)[0] + "  " + CalcArray(arr3)[1] + "  " + CalcArray(arr3)[2] + "  " + CalcArray(arr3)[3]);
+
+
+            //练习四
+            //try
+            //{
+            //    uint a = uint.Parse(Console.ReadLine());
+            //    PrimeNumber(a);
+            //}
+            //catch (Exception)
+            //{
+
+            //    Console.WriteLine("请输入大于0的正数");
+            //}
+
+            //练习五
+            try
+            {
+                LeapYear(uint.Parse(Console.ReadLine()));
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("请输入正确的年份");
+            }
+
+        }
+    }
+}
